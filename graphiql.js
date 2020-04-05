c1da5395f47f38a30655618e6fe67628cc768ce2
Leo <losoviz@gmail.com>
2020-04-05 09:40:10 +0800

    Added param "edit_schema"

--- graphiql.js
+++ graphiql.js
@@ -121,6 +121,11 @@ if ((parameters.show_logs && strToBool(parameters.show_logs)) || (scriptParams.h
   apiURL += (apiURLHasParams ? '&' : '?') + 'actions[]=show-logs';
   apiURLHasParams = true;
 }
+// Provide "edit_schema" param either through URL or through script source
+if ((parameters.edit_schema && strToBool(parameters.edit_schema)) || (scriptParams.has('edit_schema') && strToBool(scriptParams.get('edit_schema')))) {
+  apiURL += (apiURLHasParams ? '&' : '?') + 'edit_schema=true';
+  apiURLHasParams = true;
+}
 
 // Defines a GraphQL fetcher using the fetch API. You're not required to
 // use fetch, and could instead implement graphQLFetcher however you like,
